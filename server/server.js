101d2378422d2c2cbca32f6ea3ec0d405a23f92b
biplove subedi <subedi.biplove10@gmail.com>
2024-05-22 14:37:49 +0545

    dotenv installed and authRouter created

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-const exp = require('constants')
-const express = require('express')
-
+import express from 'express'
 const app = express()
+import dotenv from 'dotenv'
+import authRouter from './routes/auth.routes.js'
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
-app.get('/',(req,res)=>
-{
-    res.send("Hello world.")
-})
+app.use('/api/auth',authRouter)
 
 app.listen(5000,()=>{
-    console.log("Server is running on port 5000")
+    console.log(`Server is running on port ${PORT}`)
 })
