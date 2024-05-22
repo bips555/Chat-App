d056e0b77266bb047f62d78851fbd89afc40f589
biplove subedi <subedi.biplove10@gmail.com>
2024-05-22 14:26:40 +0545

    backend server created

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const exp = require('constants')
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>
+{
+    res.send("Hello world.")
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+})
