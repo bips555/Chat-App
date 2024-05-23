6718227187e31463e11ce1c4753d99e68662311c
biplove subedi <subedi.biplove10@gmail.com>
2024-05-23 17:23:59 +0545

    cookie parser added message and user controller finished protect route middlware added and routes alsoadded for user and messages respectively

--- server/server.js
+++ server/server.js
@@ -2,12 +2,21 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.routes.js'
+import messageRouter from './routes/message.routes.js'
+import userRouter from './routes/user.routes.js'
 import connectToDB from './db/connectToMongoDB.js'
-import exp from 'constants'
+import cookieParser from 'cookie-parser'
+
 
 dotenv.config()
 app.use(express.json())
+app.use(cookieParser())
 const PORT = process.env.PORT || 5000
+
+app.use('/api/auth',authRouter)
+
+app.use('/api/messages',messageRouter)
+app.use('/api/users',userRouter)
 
 app.use('/api/auth',authRouter)
 
