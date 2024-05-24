757d4f6c66720f3f747d16452258eaa06050f826
biplove subedi <subedi.biplove10@gmail.com>
2024-05-24 15:09:14 +0545

    searchInput made

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,15 @@
+import Conversations from "./Conversations.jsx";
+import LogoutButton from "./LogoutButton.jsx";
+import SearchInput from "./SearchInput.jsx";
+
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return <div className="border-r border-slate-500 p-4 flex flex-col">
+    <SearchInput/>
+    <div className="divider px-3">
+      <Conversations/>
+      <LogoutButton/>
+    </div>
+  </div>;
 };
 
 export default Sidebar;
