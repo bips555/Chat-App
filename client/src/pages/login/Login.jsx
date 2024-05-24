eea40c05c9e08180275dec42325d791f09725b41
biplove subedi <subedi.biplove10@gmail.com>
2024-05-24 16:50:03 +0545

    login completed and zustand implemented

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import useLogin from "../../hooks/useLogin";
 const Login = () => {
     const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-	
+	const { loading, login } = useLogin();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await login(username, password);
+	};
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -13,7 +17,7 @@ const Login = () => {
 					<span className='text-blue-500'> ChatApp</span>
 				</h1>
 
-				<form >
+				<form  onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
 							<span className='text-base label-text'>Username</span>
@@ -44,8 +48,8 @@ const Login = () => {
 					</Link>
 
 					<div>
-                    <button className='btn btn-block btn-sm mt-2' disabled={false}>
-							<span className='loading loading-spinner '>login</span>
+						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
+							{loading ? <span className='loading loading-spinner '></span> : "Login"}
 						</button>
 					</div>
 				</form>
